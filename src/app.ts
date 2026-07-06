898b0757323d38308159b9007ab7bfcfda76ed3f
Tanjim Rahat <tanjim.rahat.dipto@gmail.com>
2026-07-06 23:04:07 +0600

    Created and added user router file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { type Application } from "express";
 import cors from "cors";
 
+import userRouter from "./modules/user/user.route";
+
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/user", userRouter);
 
 export default app;
