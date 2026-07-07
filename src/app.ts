b0bf57c3ad2993461e5b2302013dd207bd812db2
Tanjim Rahat <tanjim.rahat.dipto@gmail.com>
2026-07-07 12:22:24 +0600

    added use of cookie parser middleware

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express, { type Application } from "express";
 import cors from "cors";
 
 import userRouter from "./modules/user/user.route";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 
