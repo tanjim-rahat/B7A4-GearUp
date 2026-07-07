22eb58cd24758154d0573c1ad5b780f329fd5f52
Tanjim Rahat <tanjim.rahat.dipto@gmail.com>
2026-07-07 12:18:28 +0600

    Added Jwt refresh token secrets

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,5 +8,8 @@ export const config = {
   DATABASE_URL: process.env.DATABASE_URL || "",
   JWT_SECRET: process.env.JWT_SECRET || "",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
+  JWT_REFRESH_SECRET:
+    process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET || "",
+  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
   NODE_ENV: process.env.NODE_ENV || "development",
 };
